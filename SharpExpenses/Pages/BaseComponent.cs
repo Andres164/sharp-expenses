c287db0a80ec0a6627e50e221bbd8577b91f98f2
Andres Antonio Castro Beltran <andres435b@gmail.com>
2023-06-25 20:22:14 -0700

    Made a base component for commonly used methods

---
+++ SharpExpenses/Pages/BaseComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace SharpExpenses.Pages
+{
+    public class BaseComponent : ComponentBase
+    {
+        [Inject]
+        public NotificationService NotificationService { get; set; } = null!;
+        [Inject]
+        public NavigationManager UriHelper { get; set; } = null!;
+
+        protected void ShowNotification(string message, NotificationSeverity severity = NotificationSeverity.Info)
+        {
+            this.NotificationService.Notify(new NotificationMessage { Severity = severity, Summary = message, Duration = 4000 });
+        }
+
+        protected void ReloadPage()
+        {
+            var timer = new Timer(new TimerCallback(_ =>
+            {
+                this.UriHelper.NavigateTo(this.UriHelper.Uri, forceLoad: true);
+            }), null, 2000, 2000);
+        }
+    }
+}
