6ab76c77a8bd50db1c2cebbb4379d410ebf66af4
Andres Antonio Castro Beltran <andres435b@gmail.com>
2023-06-26 10:17:16 -0700

    New ExpenseFormLayout shared component * RegisterExpense now uses the shared ExpenseFormLayout + New ModifyExpense, still working on it

---
+++ SharpExpenses/Pages/ModifyExpenseBase.cs
@@ -0,0 +1,16 @@
+﻿using SharpExpenses.Shared;
+
+namespace SharpExpenses.Pages
+{
+    public class ModifyExpenseBase : ExpenseFormLayout
+    {
+        protected override void InitializeFormExpense()
+        {
+            base.InitializeFormExpense();
+        }
+        protected override Task HandleValidSubmit()
+        {
+            return base.HandleValidSubmit();
+        }
+    }
+}
---
+++ SharpExpenses/Pages/RegisterExpenseBase.cs
@@ -0,0 +1,23 @@
+﻿using Radzen;
+using SharpExpenses.Shared;
+
+namespace SharpExpenses.Pages
+{
+    public class RegisterExpenseBase : ExpenseFormLayout
+    {
+        override protected async Task HandleValidSubmit()
+        {
+            try
+            {
+                await this.ExpensesService.Create(this._formExpense);
+                this.ShowNotification("Gasto registrado exitosamente", NotificationSeverity.Success);
+            }
+            catch (Exception ex)
+            {
+                this.ShowNotification($"Error inesperado: {ex.Message}", NotificationSeverity.Error);
+                this.ReloadPage();
+            }
+            this.InitializeFormExpense();
+        }
+    }
+}
