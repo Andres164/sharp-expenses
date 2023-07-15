16ff0a35aeab8eaae7dd22be39256f45208545ee
Andres Antonio Castro Beltran <andres435b@gmail.com>
2023-07-14 22:13:27 -0700

    Added AuthenticationService and interface classes

---
+++ SharpExpenses/Services/ApiServices/AuthenticationService.cs
@@ -0,0 +1,25 @@
+﻿using libre_pensador_api.Models.RequestModels;
+using SharpExpenses.Services.ApiServices.Contracts;
+
+namespace SharpExpenses.Services.ApiServices
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AuthenticationService(IHttpClientFactory httpClientFactory) 
+        { 
+
+        }
+
+        public Task Authenticate(UserCredentials userCredentials)
+        {
+            
+        }
+
+        public Task<bool> IsSessionAuthenticated()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SharpExpenses/Services/ApiServices/Contracts/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using libre_pensador_api.Models.RequestModels;
+
+namespace SharpExpenses.Services.ApiServices.Contracts
+{
+    public interface IAuthenticationService
+    {
+        Task Authenticate(UserCredentials userCredentials);
+        Task<bool> IsSessionAuthenticated();
+    }
+}
