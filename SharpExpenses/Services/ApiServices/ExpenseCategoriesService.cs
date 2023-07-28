b8c2655bae185879fa5c7829a6d41df4c45433a1
Andres Antonio Castro Beltran <andres435b@gmail.com>
2023-07-27 21:51:09 -0700

    Done ExpenseCategoryService, needs testing

--- SharpExpenses/Services/ApiServices/ExpenseCategoriesService.cs
+++ SharpExpenses/Services/ApiServices/ExpenseCategoriesService.cs
@@ -18,7 +18,7 @@ namespace SharpExpenses.Services.ApiServices
 
         public async Task<List<ExpenseCategory>> ReadAll()
         {
-            var response = await _httpClient.GetAsync(ControllerEndpoint);
+            var response = await _httpClient.GetAsync(this.ControllerEndpoint);
             response.EnsureSuccessStatusCode();
             var categories = await response.Content.ReadFromJsonAsync<List<ExpenseCategory>>();
             return categories!; // categories cannot be null, as the API always return a list
@@ -26,24 +26,44 @@ namespace SharpExpenses.Services.ApiServices
 
         public async Task<ExpenseCategoryViewModel?> Read(int categoryId)
         {
-            throw new NotImplementedException();
+            var response = await this._httpClient.GetAsync($"{this.ControllerEndpoint}/{categoryId}");
+
+            if(response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                return null;
+
+            response.EnsureSuccessStatusCode();
+            var category = await response.Content.ReadFromJsonAsync<ExpenseCategoryViewModel>();
+            return category;
         }
 
         public async Task Create(ExpenseCategoryViewModel newCategory)
         {
-            var response = await this._httpClient.PostAsJsonAsync(ControllerEndpoint, newCategory);
+            var response = await this._httpClient.PostAsJsonAsync(this.ControllerEndpoint, newCategory);
             response.EnsureSuccessStatusCode();
         }
 
         public async Task<ExpenseCategory?> Update(int categoryId, ExpenseCategoryViewModel updatedCategory)
         {
-            var response = await this._httpClient.PutAsJsonAsync($"{ControllerEndpoint}/{categoryId}", updatedCategory);
-            var updatedCategory = 
+            var response = await this._httpClient.PutAsJsonAsync($"{this.ControllerEndpoint}/{categoryId}", updatedCategory);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                return null;
+
+            response.EnsureSuccessStatusCode();
+            var responseUpdatedCat = await response.Content.ReadFromJsonAsync<ExpenseCategory>();
+            return responseUpdatedCat;
         }
 
         public async Task<ExpenseCategory?> Delete(int categoryId)
         {
-            throw new NotImplementedException();
+            var response = await this._httpClient.DeleteAsync($"{this.ControllerEndpoint}/{categoryId}");
+
+            if(response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                return null;
+
+            response.EnsureSuccessStatusCode();
+            var deletedCategory = await response.Content.ReadFromJsonAsync<ExpenseCategory>();
+            return deletedCategory;
         }
     }
 }
